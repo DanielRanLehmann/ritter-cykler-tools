989d5ad218030c8ad2409aa41fe2f15d849e9b5a
Daniel Ran Lehmann <danielran11@gmail.com>
2018-04-14 12:44:22 +0200

    updated reservation model

--- src/database/database.py
+++ src/database/database.py
@@ -21,37 +21,50 @@ def main():
         auth = firebase.auth()
 
         p = optparse.OptionParser()
-        p.add_option('--set', '-s', type="str", help="set data in this location in the [firebase] database")
-        p.add_option('--push', '-p', type="str", help="push data in this location in the [firebase] database.")
+        p.add_option('--read', '-r', type="str", help="read data at this location in the [firebase] database.")
+        p.add_option('--set', '-s', type="str", help="set data at this location in the [firebase] database")
+        p.add_option('--push', '-p', type="str", help="push data at this location in the [firebase] database.")
         p.add_option('--email', type="str", help="firebase auth email")
         p.add_option('--password', type="str", help="firebase auth password")
         options, arguments = p.parse_args()
 
+        user = None
+        idToken = None
         if options.email and options.password:
             user = auth.sign_in_with_email_and_password(options.email, options.password)
-            if user is None:
-                raise ValueError("User is not signed in. Please sign in and make sure you have the right permissions.")
+            idToken = user["idToken"]
 
-            if options.set:
-                with open(os.path.join(arguments[0]), 'rb+') as f:
+        #if user is None:
+        #    raise ValueError("User is not signed in. Please sign in and make sure you have the right permissions.")
+
+
+        if options.read:
+            response = db.child(options.read.strip("/")).get(idToken)
+            if response is None:
+                print "an error occurred"
+            else:
+                print json.dumps(response.val(), indent=4, sort_keys=True)
+
+        if options.set:
+            with open(os.path.join(arguments[0]), 'rb+') as f:
+                data = json.load(f)
+                location = options.set
+                print db.child(location).set(data, idToken)
+
+        elif options.push:
+            location = options.push
+            location = location.replace("/", "") # why not use strip here?
+
+            data = {}
+
+            # FILEPATH IS GIVEN AS ARGUMENT
+            if bool(arguments) is True:
+                with open(arguments[0], 'rb+') as f:
                     data = json.load(f)
-                    location = options.set
-                    print db.child(location).set(data, user["idToken"])
+                    print db.child(location).push(data, idToken) # DUPLICATE
 
-            elif options.push:
-                location = options.push
-                location = location.replace("/", "")
-
-                data = {}
-
-                # FILEPATH IS GIVEN AS ARGUMENT
-                if bool(arguments) is True:
-                    with open(arguments[0], 'rb+') as f:
-                        data = json.load(f)
-                        print db.child(location).push(data, user["idToken"]) # DUPLICATE
-
-        else:
-            print "--email and --password options are missing."
+        #else:
+        #    print "--email and --password options are missing."
 
 if __name__ == "__main__":
     main()
